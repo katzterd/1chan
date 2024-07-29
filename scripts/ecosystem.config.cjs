f953f2a757bdbacf251ecf6ed8c478b662c81933
Juribiyan <smugleaf@ya.ru>
2024-07-29 12:06:46 +0500

    daemonize with pm2

---
+++ scripts/ecosystem.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    name: '1chan-service',
+    script: 'index.js',
+    watch: false
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};

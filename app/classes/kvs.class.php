7ccefdd0c003774a7d85dfd2c793d07827564b00
Juribiyan <smugleaf@ya.ru>
2024-09-03 21:47:17 +0500

    Add extra features to the KVS class

--- app/classes/kvs.class.php
+++ app/classes/kvs.class.php
@@ -440,6 +440,10 @@ class KVS
 	 */
 	public function sortedListAdd($class, $id = null, $name = null, $value = null, $score = null)
 	{
+		if (is_null($score)) {
+			$max = $this -> sortedListMaxScore($class, $id, $name);
+			$score = is_null($max) ? 0 : ($max + 1);
+		}
 		return $this -> connection -> zadd(
 			$this -> getNS($class, $id, $name), $score, $value
 		);
@@ -457,6 +461,44 @@ class KVS
 		return $this -> connection -> zrem(
 			$this -> getNS($class, $id, $name), $value
 		);
+	}
+
+	/**
+	 * Получение рейтинга в сортированном списке:
+	 * $class  - имя класса.
+	 * $id     - идентификатор.
+	 * $name   - имя свойства.
+	 * $value  - значение.
+	 */
+	public function sortedListScore($value, $class, $id = null, $name = null) {
+		return $this -> connection -> zscore(
+			$this -> getNS($class, $id, $name), $value
+		);
+	}
+
+	/**
+	 * Проверка на вхождение в сортированный список:
+	 * $class  - имя класса.
+	 * $id     - идентификатор.
+	 * $name   - имя свойства.
+	 * $value  - значение.
+	 */
+	public function sortedListIsMember($value, $class, $id = null, $name = null) {
+		return ($this -> sortedListScore($value, $class, $id, $name) !== false);
+	}
+
+	/**
+	 * Получение максимального рейтинга в списке:
+	 * $class  - имя класса.
+	 * $id     - идентификатор.
+	 * $name   - имя свойства.
+	 * $value  - значение.
+	 */
+	public function sortedListMaxScore($class, $id = null, $name = null) {
+		$assoc = ($this -> connection -> zrevrange(
+			$this -> getNS($class, $id, $name), 0, 0, true
+		) );
+		foreach ($assoc as $i => $v) return $v;
 	}
 
 	/**

777ae649adace1cd10102594f90799298cc4824d
Vlad Kortnev <194586003+rsddsdr@users.noreply.github.com>
2025-02-18 16:15:21 +0300

    Feature/torgate (#2)
    * torgate
    
    * quick torgate fix

--- docker/config/1chan/docker-entrypoint.sh
+++ docker/config/1chan/docker-entrypoint.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+
+if [ -z "${TORGATE_HOSTNAME}" ]; then
+    echo "TORGATE_HOSTNAME environment var is undefined, Onion header will be disabled";
+    sed -i 's/__TORGATE_HEADER__//' ./nginx.conf
+else
+    sed -i 's/__TORGATE_HEADER__/'"add_header Onion-Location http\:\/\/${TORGATE_HOSTNAME}\$request_uri;"'/' ./nginx.conf
+fi
+
 /usr/sbin/nginx -c /src/nginx.conf > /dev/stdout & \
 php-fpm7.4 -O -F --fpm-config /src/php-fpm.conf
